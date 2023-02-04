4c177f5951276242edba9a98c652a6a2286bcd28
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-02-04 17:04:50 -0300

    midi

---
+++ main.js
@@ -0,0 +1,11 @@
+function pow(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function clap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tim(){
+    document.querySelector('#som_tecla_tim').play();
+}
