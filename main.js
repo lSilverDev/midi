ca35194cacab9cda33fb2d71c79e1d281fbd4fee
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-02-04 17:13:53 -0300

    midi sons

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function pow(){
+function pom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
@@ -8,4 +8,30 @@ function clap(){
 
 function tim(){
     document.querySelector('#som_tecla_tim').play();
+}
+
+
+function puff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function splash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function toim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+
+function psh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tom(){
+    document.querySelector('#som_tecla_tom').play();
 }
